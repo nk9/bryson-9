6427c2b156d24d43c7099f7aa5807645e35066a1
nk9 <nick@kocharhook.com>
2022-12-22 23:10:45 +0000

    Random failure messages.

--- pages/api/check_numbers.js
+++ pages/api/check_numbers.js
@@ -1,8 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const MESSAGES = [
+  "Try again",
+  "Is that all you’ve got??",
+  "Nope",
+  "Nuh-uh",
+  "Pfft",
+  "I don’t think so",
+  "No way José",
+  "You can do better than that",
+  "Ask Parker, maybe he’ll know",
+  "LOL",
+  "Keep trying",
+  "As if",
+  "I’ve got all day…",
+  "Close, but no cigar",
+  "Maybe next time",
+];
+
 export default function check(req, res) {
   const { set, values } = req.query;
-  let success = { message: "Success" };
+  let success = { message: "Success!!" };
   console.log(req.query);
   console.log(process.env.SET_2_VALUES);
 
@@ -13,6 +31,8 @@ export default function check(req, res) {
   } else if (set == "3" && values == process.env.SET_3_VALUES) {
     res.status(200).json({ ...success, secret: process.env.SET_3_SECRET });
   } else {
-    res.status(401).json({ message: "Try again" });
+    res
+      .status(401)
+      .json({ message: MESSAGES[(MESSAGES.length * Math.random()) | 0] });
   }
 }
