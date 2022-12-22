c284f0c02f78fc8f679d976766fe6fb255de04de
nk9 <nick@kocharhook.com>
2022-12-22 22:58:19 +0000

    More explanation of how the chapters work.

--- pages/page3.js
+++ pages/page3.js
@@ -17,9 +17,12 @@ export default function Page3() {
           Open the envelope and solve all ten chapters. Be careful: some
           chapters have more than one page!
         </Typography>
+        <Typography variant="body1" align="left" mb={2} sx={{ width: "100%" }}>
+          The answer for each chapter is a one or two digit number.
+        </Typography>
         <Typography variant="body1" align="left" mb={6} sx={{ width: "100%" }}>
-          When you have solved a set of chapters, enter them here to check your
-          work.
+          When you have solved a set of chapters, enter them here. If they are
+          right, you can move on to the next set.
         </Typography>
         <LinkButton href="/check1">Check, Mate!</LinkButton>
       </div>
